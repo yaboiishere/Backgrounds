e1d0f1e7f00104723c8f235bbc1505bd67a9ecb9
Mihail Dobrev <mixaildobrev@gmail.com>
2020-10-10 22:19:10 +0300

    format

--- get_new_backgrounds.py
+++ get_new_backgrounds.py
@@ -1,6 +1,8 @@
-import shutil, os
+import shutil
+import os
 from PIL import Image
-oc_directory = os.path.expanduser("~") + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets"
+oc_directory = os.path.expanduser(
+    "~") + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets"
 new_directory = os.getcwd() + "\\new"
 
 
@@ -8,7 +10,7 @@ for f in os.listdir(oc_directory):
     try:
         image = Image.open(oc_directory+"\\"+f)
         witdth, height = image.size
-        if witdth >= 1920 and height >= 1080:
+         if witdth >= 1920 and height >= 1080:
             shutil.copy(oc_directory+"\\"+f, new_directory)
     except FileExistsError:
         os.rmdir(new_directory+"\\"+f)
@@ -16,12 +18,12 @@ for f in os.listdir(oc_directory):
 
 for filename in os.listdir(new_directory):
     try:
-        if filename.endswith(".jpg") or filename.endswith(".py") or os.path.isdir(filename): 
+        if filename.endswith(".jpg") or filename.endswith(".py") or os.path.isdir(filename):
             continue
         else:
-            os.rename(new_directory+"\\"+filename, new_directory+"\\"+filename + ".jpg")
+            os.rename(new_directory+"\\"+filename,
+                      new_directory+"\\"+filename + ".jpg")
             continue
     except FileExistsError:
         os.remove(new_directory+"\\"+filename)
         continue
-
